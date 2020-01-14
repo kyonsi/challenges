ce7822438209dfb81b539b03254c1bdbf6405b67
kyonsi <s07e416@gmail.com>
2020-01-14 14:59:29 +0900

    add front infrastructure

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,47 @@
 const path = require('path');
 
+var isProduction = process.argv.indexOf('-p') >= 0 || process.env.NODE_ENV === 'production';
+var sourcePath = path.join(__dirname, './src');
+var contentBase = path.join(__dirname, './public');
+
 const config = {
-  entry: './src/index.js',
+  context: sourcePath,
+  entry: './index.tsx',
+  externals: {
+    "react": "React",
+    "react-dom": "ReactDOM"
+},
   output: {
     filename: '[name].js',
-    path: path.resolve(__dirname, 'build'),
-    publicPath: 'build',
+    path: contentBase,
+    publicPath: 'public',
   },
-
+  mode: "development",
+  target: 'web',
   devtool: 'inline-source-map',
-
   devServer: {
+    contentBase: contentBase,
+    hot: true,
     inline: true,
-    host: '0.0.0.0',
-    port: 3000,
-    historyApiFallback: true,
-    disableHostCheck: true,
-    contentBase: 'public',
+    historyApiFallback: {
+      disableDotRule: true
+    },
+    stats: 'minimal',
+    clientLogLevel: 'warning'
   },
-
+  devtool: isProduction ? 'hidden-source-map' : 'cheap-module-eval-source-map',
   module: {
     rules: [
       {
-        test: /\.js$/,
-        exclude: [/node_modules/],
-        use: {
-          loader: 'babel-loader',
-        },
-      },
+        test: /\.tsx?$/,
+        use: [
+          !isProduction && {
+            loader: 'babel-loader',
+            options: { plugins: ['react-hot-loader/babel'] }
+          },
+          'ts-loader'
+        ].filter(Boolean)
+      }
     ],
   },
 };
